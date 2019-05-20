d7092e50da31602f7598a4c1b0add3f210997d6f
Dinesh Sonachalam <dsonachalam@gogoair.com>
2019-05-20 12:42:13 +0530

    added dsl groovy and shell scripts

--- runway/dsl.groovy
+++ runway/dsl.groovy
@@ -1,39 +1,18 @@
-
-
-/*
-
-def project = 'nodejs-docker-demo'
-def jobName = 'nodejs-demo'
-
-job(jobName) {
-    scm {
-        git("git://github.com/sandjaie/${project}.git") {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('sandjaie')
-            node / gitConfigEmail('sandjaie@gmail.com')
+def myjob = pipelineJob('Job DSL Plugin Test') {
+    parameters {
+        stringParam('INPUT', '', 'csv')
+    }
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        name('origin')
+                        url('https://github.com/dineshsonachalam/Jenkins-Job-DSL-Plugin-Test.git')
+                    }
+                }
+            }
+            scriptPath('runway/Jenkinsfile')
         }
-    }
-    triggers {
-        scm('H/5 * * * *')
-    }
-    wrappers {
-        nodejs('nodejs') // this is the name of the NodeJS installation in 
-                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
-    }
-    steps {
-        shell("npm install")
-    }
-}
-*/
-
-job('DSL Plugin Demo') {
-    logRotator(-1, 10)
-    jdk('Java 6')
-    scm {
-        github('https://github.com/dineshsonachalam/Jenkins-Job-DSL-Plugin-Test', 'master')
-    }
-    steps {
-        sh build.sh
-        sh test.sh
-        sh deploy.sh
     }
 }
